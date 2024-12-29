4bf05c4dbf2dfaaf36eed1c2f725e7bfbe77bec1
Sebastian Carrera <scrv@Sebastians-MacBook-Pro.local>
2024-12-29 14:27:44 +0900

    style: footer styling and linking

--- src/.vitepress/theme/Layout.vue
+++ src/.vitepress/theme/Layout.vue
@@ -13,5 +13,34 @@ const { site, frontmatter } = useData()
       <span>{{ frontmatter.breadcrumb }}</span>
     </div>
   </header>
-  <Content />
+  <main>
+    <Content />
+  </main>
+  <hr>
+  <footer>
+    <nav>
+      <div>
+        <a href="/about">About</a>
+        <a href="/work">Work</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div>
+        <a href="/photos">Photos</a>
+        <a href="/notes">Notes</a>
+        <a href="/books">Books</a>
+        <a href="/things">Things</a>
+      </div>
+    </nav>
+
+    <section>
+      <img src="" alt="">
+
+      <div>
+        <a href="/colophon">Colophon</a>
+        <span> · </span>
+        <a href="mailto:sbcrvl@gmail.com">Mail</a>
+      </div>
+    </section>
+  </footer>
 </template>
